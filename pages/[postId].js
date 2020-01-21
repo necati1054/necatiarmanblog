f2b95083aa6491a6e9fe9f5a7662f3d967542158
necati1054 <59114685+necati1054@users.noreply.github.com>
2020-01-22 01:29:18 +0300

    aaaa

--- pages/[postId].js
+++ pages/[postId].js
@@ -12,6 +12,7 @@ const BlogPost = ({ post }) => (
       <link rel="icon" href="/favicon.ico" />
       <script data-ad-client="ca-pub-2037585543989384" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <meta charSet="utf-8"></meta>
+      
     </Head>
 
     <Menu></Menu>
