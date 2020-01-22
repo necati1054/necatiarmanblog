1313ecb9aa6614dc15fb1445266c496c7278d66d
necati1054 <59114685+necati1054@users.noreply.github.com>
2020-01-22 11:43:06 +0300

    hadi be

--- pages/contac.js
+++ pages/contac.js
@@ -144,7 +144,7 @@ const contac = () => (
 );
 
 contac.getInitialProps = async ({ req, query }) => {
-  const res = await fetch(`http://localhost:3000/api/post/${query.postId}`);
+  const res = await fetch(`https://necatiarmanblog.herokuapp.com/api/post/${query.postId}`);
   const json = await res.json();
   return { post: json.post };
 };
