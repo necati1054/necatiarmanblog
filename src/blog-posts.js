480f932f24482964d2f96ac11dc0eb8a41f1a922
necati1054 <59114685+necati1054@users.noreply.github.com>
2020-02-03 22:44:23 +0300

    update

--- src/blog-posts.js
+++ src/blog-posts.js
@@ -1,6 +1,22 @@
 export const getPosts = () => {
   return [
     {
+      title: "Epic Games",
+      slug: "EpicGames",
+      details: "Epic Games üzerinden Bedava oynaya bileceğiniz oyunlar",
+      date: "01 Şubat 2020",
+      link: require("./posts/EpicGames.md").default,
+      photo: "https://iasbh.tmgrup.com.tr/250b3c/752/395/0/26/739/415?u=https://isbh.tmgrup.com.tr/sbh/2019/12/20/epic-games-storedan-buyuk-kampanya-epic-gameste-60-liralik-indirim-kodu-ve-cok-sayida-oyun-bedava-1576830918786.jpg"
+    },
+    {
+      title: "Farming Simulator 19",
+      slug: "FarmingSimulator19",
+      details: "FarmingSimulator19 bedava oldu ",
+      date: "31 Ocak 2020",
+      link: require("./posts/FarmingSimulator19.md").default,
+      photo: "https://steamcdn-a.akamaihd.net/steam/apps/787860/header.jpg?t=1578412042"
+    },
+    {
       title: "Instagram Hashtagleri",
       slug: "instagram",
       details: "En Popüler Instagram Hashtagleri.",
